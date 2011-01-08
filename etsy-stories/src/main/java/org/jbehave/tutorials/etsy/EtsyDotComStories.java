21b8537013bc059e899f4f0a88e552bfa4a9c965
Mauro Talevi <mauro.talevi@aquilonia.org>
2011-01-08 22:55:00 +0000

    Close down context view after stories.

--- etsy-stories/src/main/java/org/jbehave/tutorials/etsy/EtsyDotComStories.java
+++ etsy-stories/src/main/java/org/jbehave/tutorials/etsy/EtsyDotComStories.java
@@ -1,6 +1,12 @@
 package org.jbehave.tutorials.etsy;
 
 import groovy.lang.MetaClass;
+
+import java.lang.reflect.Method;
+import java.util.List;
+import java.util.Properties;
+
+import org.jbehave.core.annotations.AfterStories;
 import org.jbehave.core.configuration.Configuration;
 import org.jbehave.core.configuration.groovy.GroovyResourceFinder;
 import org.jbehave.core.failures.FailingUponPendingStep;
@@ -37,10 +43,6 @@ import org.picocontainer.injectors.ConstructorInjection;
 import org.picocontainer.injectors.SetterInjection;
 import org.picocontainer.injectors.SetterInjector;
 
-import java.lang.reflect.Method;
-import java.util.List;
-import java.util.Properties;
-
 import static java.util.Arrays.asList;
 import static org.jbehave.core.io.CodeLocations.codeLocationFromClass;
 import static org.jbehave.core.reporters.Format.CONSOLE;
@@ -52,9 +54,8 @@ public class EtsyDotComStories extends JUnitStories {
 
     private WebDriverProvider driverProvider = new TypeWebDriverProvider();
     private Configuration configuration;
-    private static ContextView contextView = new LocalFrameContextView().sized(640,120);
+    private static ContextView contextView = new LocalFrameContextView().sized(640, 120);
     private static SeleniumContext seleniumContext = new SeleniumContext();
-
 
     @Override
     public Configuration configuration() {
@@ -65,49 +66,56 @@ public class EtsyDotComStories extends JUnitStories {
     public static Configuration makeConfiguration(Class<?> embeddableClass, WebDriverProvider driverProvider) {
 
         return new SeleniumConfiguration()
-            .useWebDriverProvider(driverProvider)
-            .useSeleniumContext(seleniumContext)
-            .useFailureStrategy(new FailingUponPendingStep())
-            .useStepMonitor(new SeleniumStepMonitor(contextView, new SeleniumContext(), new SilentStepMonitor()))
-            .useStoryLoader(new LoadFromClasspath(embeddableClass.getClassLoader()))
-            .useStoryReporterBuilder(
-                new StoryReporterBuilder()
-                    .withCodeLocation(CodeLocations.codeLocationFromClass(embeddableClass))
-                    .withDefaultFormats()
-                    .withFormats(new SeleniumContextOutput(seleniumContext), CONSOLE, TXT, HTML, XML));
+                .useWebDriverProvider(driverProvider)
+                .useSeleniumContext(seleniumContext)
+                .useFailureStrategy(new FailingUponPendingStep())
+                .useStepMonitor(new SeleniumStepMonitor(contextView, new SeleniumContext(), new SilentStepMonitor()))
+                .useStoryLoader(new LoadFromClasspath(embeddableClass.getClassLoader()))
+                .useStoryReporterBuilder(
+                        new StoryReporterBuilder()
+                                .withCodeLocation(CodeLocations.codeLocationFromClass(embeddableClass))
+                                .withDefaultFormats()
+                                .withFormats(new SeleniumContextOutput(seleniumContext), CONSOLE, TXT, HTML, XML));
     }
 
     @Override
     public List<CandidateSteps> candidateSteps() {
-        List<CandidateSteps> steps = makeGroovyCandidateSteps(configuration(), new GroovyResourceFinder(), driverProvider);
-        steps.add(0, stepify(new PerStoriesWebDriverSteps(driverProvider))); // before other Groovy steps
+        List<CandidateSteps> steps = makeGroovyCandidateSteps(configuration(), new GroovyResourceFinder(),
+                driverProvider);
+        steps.add(0, stepify(new PerStoriesWebDriverSteps(driverProvider))); // before
+                                                                             // other
+                                                                             // Groovy
+                                                                             // steps
         steps.add(stepify(new WebDriverScreenshotOnFailure(driverProvider)));
-
+        steps.add(stepify(new PerStoriesContextView()));
         return steps;
     }
 
-    private Steps stepify(final Object steps) {
-        return new Steps(configuration, steps);
+    private Steps stepify(final Object object) {
+        return new Steps(configuration, object);
     }
 
-    public static List<CandidateSteps> makeGroovyCandidateSteps(final Configuration configuration, GroovyResourceFinder resourceFinder, final WebDriverProvider webDriverProvider) {
+    @SuppressWarnings("serial")
+    public static List<CandidateSteps> makeGroovyCandidateSteps(final Configuration configuration,
+            GroovyResourceFinder resourceFinder, final WebDriverProvider webDriverProvider) {
 
-        ClassLoadingPicoContainer comps = new DefaultClassLoadingPicoContainer(
-                new CompositeInjection(
-                        new ConstructorInjection(),
-                        new SetterInjection(){
+        ClassLoadingPicoContainer comps = new DefaultClassLoadingPicoContainer(new CompositeInjection(
+                new ConstructorInjection(), new SetterInjection() {
+                    @Override
+                    public <T> ComponentAdapter<T> createComponentAdapter(ComponentMonitor monitor,
+                            LifecycleStrategy lifecycleStrategy, Properties componentProperties, Object componentKey,
+                            Class<T> componentImplementation, Parameter... parameters) throws PicoCompositionException {
+                        return new SetterInjector<T>(componentKey, componentImplementation, parameters, monitor, "set",
+                                true) {
                             @Override
-                            public <T> ComponentAdapter<T> createComponentAdapter(ComponentMonitor monitor, LifecycleStrategy lifecycleStrategy, Properties componentProperties, Object componentKey, Class<T> componentImplementation, Parameter... parameters) throws PicoCompositionException {
-                                return new SetterInjector<T>(componentKey, componentImplementation, parameters, monitor, "set", true) {
-                                    @Override
-                                    protected boolean isInjectorMethod(Method method) {
-                                        // this could go into PicoContainer itself
-                                        boolean b = method.getParameterTypes()[0] != MetaClass.class;
-                                        return b && super.isInjectorMethod(method);
-                                    }
-                                };
+                            protected boolean isInjectorMethod(Method method) {
+                                // this could go into PicoContainer itself
+                                boolean b = method.getParameterTypes()[0] != MetaClass.class;
+                                return b && super.isInjectorMethod(method);
                             }
-                        }));
+                        };
+                    }
+                }));
         comps.change(Characteristics.USE_NAMES);
         comps.addComponent(WebDriverProvider.class, webDriverProvider);
         comps.addComponent(new ClassName("pages.AdvancedSearch"));
@@ -128,18 +136,14 @@ public class EtsyDotComStories extends JUnitStories {
     @Override
     protected List<String> storyPaths() {
         return new StoryFinder()
-                .findPaths(codeLocationFromClass(this.getClass())
-                        .getFile(), asList("**/*.story"), null);
+                .findPaths(codeLocationFromClass(this.getClass()).getFile(), asList("**/*.story"), null);
     }
+    
+    
+    public static class PerStoriesContextView {
 
-    /**
-     * This could go into JBehave-Web perhaps.
-     */
-    @Override
-    public void run() throws Throwable {
-        try {
-            super.run();
-        } finally {
+        @AfterStories
+        public void afterStory() {
             contextView.close();
         }
     }
