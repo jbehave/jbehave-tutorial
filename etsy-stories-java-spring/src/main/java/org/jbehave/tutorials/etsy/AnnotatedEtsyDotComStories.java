3bd36faf05ed03b802e04c48efdeee4b46e71ce8
Mauro Talevi <mauro.talevi@aquilonia.org>
2011-05-31 19:27:42 +0200

    Added example of running stories via AnnotatedEmbedder.

---
+++ etsy-stories-java-spring/src/main/java/org/jbehave/tutorials/etsy/AnnotatedEtsyDotComStories.java
@@ -0,0 +1,72 @@
+package org.jbehave.tutorials.etsy;
+
+import java.util.List;
+
+import org.jbehave.core.InjectableEmbedder;
+import org.jbehave.core.annotations.Configure;
+import org.jbehave.core.annotations.UsingEmbedder;
+import org.jbehave.core.annotations.spring.UsingSpring;
+import org.jbehave.core.configuration.Configuration;
+import org.jbehave.core.embedder.Embedder;
+import org.jbehave.core.embedder.StoryControls;
+import org.jbehave.core.failures.FailingUponPendingStep;
+import org.jbehave.core.io.LoadFromClasspath;
+import org.jbehave.core.io.StoryFinder;
+import org.jbehave.core.junit.spring.SpringAnnotatedEmbedderRunner;
+import org.jbehave.core.reporters.CrossReference;
+import org.jbehave.core.reporters.Format;
+import org.jbehave.core.reporters.StoryReporterBuilder;
+import org.jbehave.web.selenium.ContextView;
+import org.jbehave.web.selenium.LocalFrameContextView;
+import org.jbehave.web.selenium.SeleniumConfiguration;
+import org.jbehave.web.selenium.SeleniumContext;
+import org.jbehave.web.selenium.SeleniumContextOutput;
+import org.jbehave.web.selenium.SeleniumStepMonitor;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static java.util.Arrays.asList;
+import static org.jbehave.core.io.CodeLocations.codeLocationFromClass;
+import static org.jbehave.core.reporters.Format.CONSOLE;
+import static org.jbehave.web.selenium.WebDriverHtmlOutput.WEB_DRIVER_HTML;
+
+@RunWith(SpringAnnotatedEmbedderRunner.class)
+@Configure(implementation = SeleniumConfiguration.class, pendingStepStrategy = FailingUponPendingStep.class)
+@UsingEmbedder(embedder = Embedder.class, generateViewAfterStories = true, ignoreFailureInStories = true, ignoreFailureInView = false, storyTimeoutInSecs = 100, threads = 1, metaFilters = "-skip")
+@UsingSpring(resources = { "etsy-steps.xml" })
+public class AnnotatedEtsyDotComStories extends InjectableEmbedder {
+
+    public AnnotatedEtsyDotComStories() {
+    }
+
+    @Test
+    public void run() throws Throwable {
+        CrossReference crossReference = new CrossReference().withJsonOnly().withOutputAfterEachStory(true)
+                .excludingStoriesWithNoExecutedScenarios(true);
+        ContextView contextView = new LocalFrameContextView().sized(640, 120);
+        SeleniumContext seleniumContext = new SeleniumContext();
+        SeleniumStepMonitor stepMonitor = new SeleniumStepMonitor(contextView, seleniumContext,
+                crossReference.getStepMonitor());
+        Format[] formats = new Format[] { new SeleniumContextOutput(seleniumContext), CONSOLE, WEB_DRIVER_HTML };
+        StoryReporterBuilder reporterBuilder = new StoryReporterBuilder()
+                .withCodeLocation(codeLocationFromClass(EtsyDotComStories.class)).withFailureTrace(true)
+                .withFailureTraceCompression(true).withDefaultFormats().withFormats(formats)
+                .withCrossReference(crossReference);
+
+        Configuration configuration = injectedEmbedder().configuration();
+        configuration.useFailureStrategy(new FailingUponPendingStep())
+                .useStoryControls(new StoryControls().doResetStateBeforeScenario(false)).useStepMonitor(stepMonitor)
+                .useStoryLoader(new LoadFromClasspath(EtsyDotComStories.class))
+                .useStoryReporterBuilder(reporterBuilder);
+        if (configuration instanceof SeleniumConfiguration) {
+            SeleniumConfiguration seleniumConfiguration = (SeleniumConfiguration) configuration;
+            seleniumConfiguration.useSeleniumContext(seleniumContext);
+        }
+        injectedEmbedder().runStoriesAsPaths(storyPaths());
+    }
+
+    protected List<String> storyPaths() {
+        return new StoryFinder().findPaths(codeLocationFromClass(this.getClass()).getFile(),
+                asList("**/" + System.getProperty("storyFilter", "*") + ".story"), null);
+    }
+}
