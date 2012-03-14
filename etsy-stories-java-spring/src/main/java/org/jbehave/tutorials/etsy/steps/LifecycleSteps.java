7fb813f14c14c4346214b7e14b9b6b9f46521fb9
Paul Hammant <paul@hammant.org>
2012-03-14 09:27:28 -0500

    change to use PerStories webdriver, because of spring singleton scope

--- etsy-stories-java-spring/src/main/java/org/jbehave/tutorials/etsy/steps/LifecycleSteps.java
+++ etsy-stories-java-spring/src/main/java/org/jbehave/tutorials/etsy/steps/LifecycleSteps.java
@@ -1,22 +1,48 @@
 package org.jbehave.tutorials.etsy.steps;
 
+import org.jbehave.core.annotations.AfterScenario;
+import org.jbehave.core.annotations.AfterStories;
+import org.jbehave.core.annotations.AfterStory;
 import org.jbehave.core.annotations.BeforeScenario;
+import org.jbehave.core.annotations.BeforeStories;
+import org.jbehave.core.annotations.BeforeStory;
+import org.jbehave.web.selenium.FirefoxWebDriverProvider;
+import org.jbehave.web.selenium.PerStoriesWebDriverSteps;
 import org.jbehave.web.selenium.PerStoryWebDriverSteps;
 import org.jbehave.web.selenium.WebDriverProvider;
 import org.openqa.selenium.WebDriverException;
 
-public class LifecycleSteps extends PerStoryWebDriverSteps {
+public class LifecycleSteps extends PerStoriesWebDriverSteps {
+
+    private static final String JOURNAL_FIREFOX_COMMANDS = System.getProperty("JOURNAL_FIREFOX_COMMANDS", "false");
 
     public LifecycleSteps(WebDriverProvider webDriverProvider) {
         super(webDriverProvider);
     }
 
-    @BeforeScenario
-    public void emptyCart() {
-        try {
-            driverProvider.get().manage().deleteCookieNamed("uaid");
-        } catch (WebDriverException e) {
-            e.printStackTrace();
-        }
+    @Override
+    @BeforeStories
+    public void beforeStories() throws Exception {
+
+        super.beforeStories();
+
+        driverProvider.get().manage().deleteCookieNamed("uaid");
     }
+
+    @Override
+    @AfterStories
+    public void afterStories() throws Exception {
+
+        if (!JOURNAL_FIREFOX_COMMANDS.equals("false") && driverProvider instanceof FirefoxWebDriverProvider) {
+            FirefoxWebDriverProvider.WebDriverJournal journal = ((FirefoxWebDriverProvider) driverProvider).getJournal();
+            System.out.println("Journal of WebDriver Commands:");
+            for (Object entry : journal) {
+                System.out.println(entry);
+            }
+            ((FirefoxWebDriverProvider) driverProvider).clearJournal();
+        }
+
+        super.afterStories();
+    }
+
 }
